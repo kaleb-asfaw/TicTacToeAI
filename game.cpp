e0a79fe71366167911c91af08299ea93396b31e6
Kaleb Asfaw <122475977+kaleb-asfaw@users.noreply.github.com>
2024-01-27 01:42:05 -0500

    Update game.cpp
    supports user selecting difficulty

--- game.cpp
+++ game.cpp
@@ -14,10 +14,7 @@ int main() {
     std::cout << std::endl;
     int turn = printInstructions(); // Instructions for how to select squares + intro message
     std::vector<std::vector<char>> board = generateBoard();
-
-    if (turn == -1){
-        exit(0);}
-
+    
     switch (turn){
         case 1:
             std::cout << "You\'ve opted to go first!" << std::endl << std::endl;
@@ -30,8 +27,30 @@ int main() {
             std::cout << "I don't know wagwan, but there was an error. Fatal.";
             exit(0);};
 
+    std::cout << "Select difficulty level (0 for easy, 1 for hard): ";
+    int diff;
+    while (true){
+        std::cin >> diff;
+        if (std::cin.fail()) {
+            std::cin.clear(); // Clear the error state
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Remove bad input
+            std::cout << "This application only accepts integer values. Please try again: ";}
+        
+        else if (diff != 0 && diff != 1){
+            std::cout << "Invalid input. Please select 0 (easy) or 1 (hard): ";}
+        else{
+            break;
+        }
+        };
+    
+    std::cout << std::endl;
+
+    if (turn == -1){
+        exit(0);}
     
     bool playerTurn = (turn == 1); 
+    bool difficulty = (diff == 1); // true is minimax, false is easy mode (random selection)
+    
     while (true){
         if (playerTurn){ // If it's the player's turn
             playerMove(board);
@@ -43,7 +62,7 @@ int main() {
             playerTurn = false;
         }
         else{ // If it's the AI's turn
-            aiMove(board, true);
+            aiMove(board, difficulty);
             if (isWin(board, 'O')){
                 printBoard(board);
                 std::cout << std::endl << "The AI wins, run program to play again.";
